ac38af83ddcfac837e4f1a50931ac6da13685712
SaneBow <sanebow@gmail.com>
2015-06-04 00:53:49 +0800

    测试mutation observer

--- wxBot.js
+++ wxBot.js
@@ -156,8 +156,7 @@ function chatbot() {
     if ( $('.activeColumn:has(".bot.active")').length ) {
         var observer = new MutationObserver(function(mutations) {
          mutations.forEach(function(mutation) {
-           for (var i = 0; i < mutation.addedNodes.length; i++)
-             console.log(mutation.addedNodes[i]);
+             console.log(mutation.addedNodes);
          });
         });
         observer.observe($('#chat_chatmsglist')[0], { childList: true});
