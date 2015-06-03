359c87b36350c537163eca849d13e59888e84b8c
SaneBow <sanebow@gmail.com>
2015-06-03 15:25:29 +0800

    js code structure reconstructed

--- wxBot.js
+++ wxBot.js
@@ -1,3 +1,7 @@
+function debug(msg){
+    DEBUG && console.log(msg);
+}
+
 function callBotAPI(newmsg,callback) {
    //YQL for cross region json ajax
     var yql_url = 'https://query.yahooapis.com/v1/public/yql';
@@ -15,20 +19,127 @@ function callBotAPI(newmsg,callback) {
             callback(ans);
         },
         'error': function() {
-            console.log("ajax error occured");
+            debug("ajax error occured");
         }
     });
 }
 
 function sendmsg(ans) {
-    console.log("bot resp with: " + ans);
+    debug("bot resp with: " + ans);
     $('#textInput')[0].value=ans;
     $('.chatSend')[0].click();
-    console.log("msg sent to: " + name);
+    debug("msg sent to: " + name);
     $('#conv_filehelper').click();
 }
 
+function installbot(chats){
+    //add bot notes
+    for (i=0;i<chats.length;i++) {
+        var bot = document.createElement('div');
+        bot.className='bot';
+        chats[i].appendChild(bot);
+    }
+
+    //listen on bot click event
+    $(chats).children('.bot').click(function(e){
+        e.stopPropagation();
+        $(this).toggleClass('active');
+    });
+
+    //add drag drop event to bot div
+    jQuery.event.props.push('dataTransfer');
+    $(chats).on({
+        dragenter: function(e) {
+            $(this).addClass('over');
+        },
+        dragover: function(e) {
+            e.preventDefault();
+        },
+        dragleave: function(e) {
+            $(this).removeClass('over');
+        },
+        drop: function() {
+            $(this).find('.bot').toggleClass('active');
+        },
+    });
+
+    debug("installed "+chats.length.toString()+" bots");
+}
+
+function buildbothome() {
+    //add bot home
+    $('#profile').append([
+    '<div id="bot-wrapper">',
+        '<img class="bot-home botico" title="wxBot - a WeChat chat bot" src="https://raw.githubusercontent.com/SaneBow/wxBot/master/icons/baymax1.png" draggable="true" />',
+        '<span class="tooltip blink">',
+            '<p>Drag & drop me - set up</p>',
+            '<p>Click me - pause/resume</p>',
+        '</span>',
+        '<img class="bot-home pauseico" src="https://raw.githubusercontent.com/SaneBow/wxBot/master/icons/pause.png" draggable="false" />',
+    '</div>'].join(''));
+
+    //show tooltip for 10s
+    $('#bot-wrapper span').show();
+    setTimeout(function(){
+        $('#bot-wrapper span').hide();
+    },10*1000);
+
+    //clear tooltip on mousedown
+    $('.bot-home').mousedown(function(){
+        $('#bot-wrapper span').hide();
+    });
+
+    //listen on bot-home click event
+    $('.bot-home').click(function(){
+        $('.bot-home').toggleClass('paused');
+    });
+
+    //listen drag event
+    jQuery.event.props.push('dataTransfer');
+    $('.bot-home').on({
+        dragstart: function() {
+            $(this).css('opacity', '0.5');
+            $('.chatListColumn *').css('pointer-events','none');
+        },
+        dragend: function() {
+            $(this).css('opacity', '1');
+            $('.chatListColumn *').css('pointer-events','');
+            $('.chatListColumn').removeClass('over');
+        },
+    });
+    debug("bot home constructed");
+}
+
+function botinit(){
+    $(document).ready(function(){
+        //init bot-home
+        buildbothome();
+        //init bot divs
+        installbot($(['.chatListColumn',
+            ':not([un="newsapp"])',
+            ':not([un="filehelper"])',
+            ':not(".loadMoreConv")'].join('')));
+    });
+    debug("bot initiated");
+}
+
+function botstart(interval){
+    setInterval(chatbot,interval*1000);
+    debug("started with interval: "+interval.toString()+"s");
+}
+
+function botupdate(){
+    uninstalled = $(['.chatListColumn',
+            ':not([un="newsapp"])',
+            ':not([un="filehelper"])',
+            ':not(".loadMoreConv")',
+            ':not(has:(".bot"))'].join(''));
+    uninstalled.length && installbot(uninstalled);
+    debug(uninstalled.length.toString()+" nodes to update");
+}
+
 function chatbot() {
+    botupdate();
     //if paused
     if ( $('.bot-home').hasClass('paused') ) return;
     //if no bot-took-overed
@@ -40,93 +151,22 @@ function chatbot() {
         is_active = $(this).parent().find(".bot").hasClass("active");
         if (!is_active) return;
         name = $(this).parent().find(".left.name").text();
-        console.log("msg from: " + name);
+        debug("msg from: " + name);
         $(this).click();
         //Wait till chat box loaded
         setTimeout(function(){
             newmsg = $("#chat_chatmsglist").children(".chatItem.you").last().find("pre").text();
             if (newmsg) {
-                console.log("msg content: " + newmsg);
+                debug("msg content: " + newmsg);
                 callBotAPI(newmsg,sendmsg);
             } else {
-                console.log("no msg found")
+                debug("no msg found")
             }
         },500);
     });
 }
 
-function botinit(){
-    //setInterval(chatbot,30*1000)
-    $(document).ready(function(){
-        //add bot notes
-        chats = document.getElementsByClassName('chatListColumn');
-        for (i=0;i<chats.length;i++) {
-            var bot = document.createElement('div');
-            bot.className='bot';
-            chats[i].appendChild(bot);
-        }
-
-        //listen on bot click event
-        $('.bot').click(function(e){
-            e.stopPropagation();
-            $(this).toggleClass('active');
-        });
-
-        //add bot home
-        $('#profile').append([
-        '<div id="bot-wrapper">',
-            '<img class="bot-home botico" title="wxBot - a WeChat chat bot" src="https://raw.githubusercontent.com/SaneBow/wxBot/master/icons/baymax1.png" draggable="true" />',
-            '<span class="tooltip blink">drag & drop me</span>',
-            '<img class="bot-home pauseico" src="https://raw.githubusercontent.com/SaneBow/wxBot/master/icons/pause.png" draggable="false" />',
-        '</div>'].join(''));
-
-        //show tooltip for 10s
-        $('#bot-wrapper span').show();
-        setTimeout(function(){
-            $('#bot-wrapper span').hide();
-        },5*1000);
-
-        $('.bot-home').mousedown(function(){
-            $('#bot-wrapper span').hide();
-        });
-
-        //listen on bot-home click event
-        $('.bot-home').click(function(){
-            $('.bot-home').toggleClass('paused');
-        });
-
-        //listen drag event
-        jQuery.event.props.push('dataTransfer');
-        $('.bot-home').on({
-            dragstart: function() {
-                $(this).css('opacity', '0.5');
-                $('.chatListColumn *').css('pointer-events','none');
-            },
-            dragend: function() {
-                $(this).css('opacity', '1');
-                $('.chatListColumn *').css('pointer-events','');
-                $('.chatListColumn').removeClass('over');
-            },
-        });
-        $('.chatListColumn').on({
-            dragenter: function(e) {
-                $(this).addClass('over');
-            },
-            dragover: function(e) {
-                e.preventDefault();
-            },
-            dragleave: function(e) {
-                $(this).removeClass('over');
-            },
-            drop: function() {
-                $(this).find('.bot').toggleClass('active');
-            },
-        });
-
-        //start bot
-        setInterval(chatbot,30*1000);
-    });
-}
-
-
+DEBUG = true;
 botinit();
+botstart(30);
+botupdate();
